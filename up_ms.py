27e62dbf3927f9282c63f21a06c562d6c94b47c1
JohnSalas8 <johnsalaslp@gmail.com>
2018-05-24 14:08:54 -0500

    Actualizando el servicio

--- up_ms.py
+++ up_ms.py
@@ -1,17 +1,24 @@
 from flask import Flask, request, render_template
 from flask_restful import Resource, Api
+from Services.SimpsonRule import SimpsonRule
+#from Services.TrapecioRule import TrapecioRule
 
 app = Flask(__name__)
 api = Api(app)
 
-@app.route('/simpson/')
-def simpson():
+@app.route('/simpson/<a>/<b>/<exp>/<n>/<h>')
+def simpson(a, b, exp, n, h):
     pass
 
-@app.route('/trapecio/')
-def trapecio():
+@app.route('/trapecio/<a>/<b>/<exp>/<n>/<h>')
+def trapecio(a, b, exp, n, h):
     pass
 
 @app.route('/')
 def index():
-    pass
+    return render_template('information.html')
+
+
+if __name__ == '__main__':
+    app.run(port=2511)
+    
